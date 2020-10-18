b5ae4bc1703b5cb81e66ce7bc85f231d0323e933
adityak-bit <adityajsr1998@gmail.com>
2020-10-19 03:32:44 +0530

    [ADITYA] Added : Ability to find the cheapest Hotel for a given Date Range

--- test/java/com/cg/HotelReservationTest.java
+++ test/java/com/cg/HotelReservationTest.java
@@ -1,5 +1,13 @@
 package com.cg;
 
-public class HotelReservationTest {
+import org.junit.*;
 
+public class HotelReservationTest {
+	
+	@Test
+	public void findCheapestHotelForGivenDateRange_ConsideringOnlyWeekdayRates() {
+		HotelReservationMain hotelReservationMain = new HotelReservationMain();
+		String hotel = hotelReservationMain.findCheapestHotelForGivenDateRangeConsideringWeekdayRatesOnly("10Sep2020","11Sep2020");
+		Assert.assertEquals("Lakewood", hotel);
+	}
 }
