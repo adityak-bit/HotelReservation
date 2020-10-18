e207b30e68b0734557b4668f52403a74243a307a
adityak-bit <adityajsr1998@gmail.com>
2020-10-19 04:39:15 +0530

    [ADITYA] Added : Ability to find the cheapest Hotel for a given date range based on weekday and weekend

--- test/java/com/cg/HotelReservationTest.java
+++ test/java/com/cg/HotelReservationTest.java
@@ -1,5 +1,9 @@
 package com.cg;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.*;
 
 public class HotelReservationTest {
@@ -9,5 +13,13 @@ public class HotelReservationTest {
 		HotelReservationMain hotelReservationMain = new HotelReservationMain();
 		String hotel = hotelReservationMain.findCheapestHotelForGivenDateRangeConsideringWeekdayRatesOnly("10Sep2020","11Sep2020");
 		Assert.assertEquals("Lakewood", hotel);
+	}
+	
+	@Test
+	public void findCheapestHotelForGivenDateRange_ConsideringBothWeekdayAndWeekendRates() {
+		HotelReservationMain hotelReservationMain = new HotelReservationMain();
+		List<String> actualList = hotelReservationMain.findCheapestHotelForGivenDateRangeConsideringWeekdayAndWeekendRatesBoth("11Sep2020", "12Sep2020");
+		List<String> expectedList = new ArrayList<>(Arrays.asList("Lakewood","Bridgewood"));
+		Assert.assertEquals(expectedList, actualList);
 	}
 }
